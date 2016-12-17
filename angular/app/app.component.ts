3b388a48a0010b46f4a398eb7c49fad3e39b49e4
Eduard Erja <eduard.erja@gmail.com>
2016-12-17 16:11:27 +0200

    Workaround for creative.js
    imported js onInit app component

--- angular/app/app.component.ts
+++ angular/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+const url = 'app/assets/js/creative.min.js';
 
 @Component({
     selector: 'healthy-app',
     moduleId: module.id,
     templateUrl: `app.component.html`,
-    
+
 })
-export class AppComponent { 
+export class AppComponent implements OnInit {
 
     pageTitle: string = 'Healthy';
     user: string = 'User';
 
-   
+    ngOnInit(): void {
+        let node = document.createElement('script');
+        node.src = url;
+        node.type = 'text/javascript';
+        node.async = true;
+        node.charset = 'utf-8';
+        document.getElementsByTagName('body')[0].appendChild(node);
+
+    }
+
+
 }
