081467cae038bd0bbd7045e7ce6cd387e106e704
Eduard Erja <eduard.erja@gmail.com>
2017-01-31 12:04:23 +0200

    Workaround for date format
    post service still not working. need authenticatd user

--- angular/app/lab/lab-add.component.ts
+++ angular/app/lab/lab-add.component.ts
@@ -29,13 +29,25 @@ export class LabAddComponent {
         patient_code: ["", Validators.required]
     });
 
+    
+    
+   
+
     constructor(public _fb: FormBuilder, private _labService: LabService) {
 
     }
 
     public saveLab() {
 
+        // Workaround #1 to format the date
+        var dateString: string = this.labForm.value.date;
+        dateString=new Date(dateString).toLocaleDateString('sq-AL');
+        dateString=dateString.split(' ').slice(0, 4).join(' ')
+        this.labForm.value.date = dateString;
+        // end of workaround #1
+        
         let newLab : ILab = this.labForm.value;
+        
         this._labService.addLab(newLab).subscribe(
             data => {
                 // this.getLabs
@@ -43,7 +55,5 @@ export class LabAddComponent {
             }
         );
 
-       // console.log(event);
-        console.log(this.labForm.value);
     }
 }
